66f8b980cdcca7ba93453baebb4c42aa76e5d422
Valentina Ciav <ciav.valentina@gmail.com>
2026-05-15 20:39:49 +0200

    Convert site to one-page layout

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 set -eu
 
 PORT="${PORT:-8000}"
+HOST="${HOST:-0.0.0.0}"
 
 cd "$(dirname "$0")"
-exec python3 -m http.server "$PORT" --directory static
+exec python3 -m http.server "$PORT" --bind "$HOST" --directory static
